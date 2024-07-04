3a15fa9583cf32dcae390343febea1fb8147d065
FTerradas <terradas.federico92@gmail.com>
2024-07-03 22:23:57 -0300

    Add files via upload

---
+++ API.Ft-Entrega-3/sql_scripts/sql_query_creation.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS federico_terradas_coderhouse.weather_data;
+
+CREATE TABLE federico_terradas_coderhouse.weather_data (
+    city VARCHAR(250),
+    temperature FLOAT,
+    humidity INT,
+    pressure INT,
+    weather VARCHAR(250),
+    wind_speed FLOAT,
+    timestamp TIMESTAMP SORTKEY,
+    PRIMARY KEY (city, timestamp)
+)
+diststyle all;
